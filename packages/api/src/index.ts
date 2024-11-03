98d033d39feca3af90046a5d53d6571990d75426
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2024-11-03 16:45:21 -0600

    infra: adds docker compose to initiate redis and kafka services

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,11 +2,8 @@
 import http from "http";
 import cors from "cors";
 import express from "express";
-import { Request, Response } from "express";
-import { Server } from "socket.io";
 
 // Shared packages
-import prismaClient from "prisma-client";
 import SocketService from "./socket";
 import { publisher, subscriber } from "pubsub";
 import { producer } from "worker-queue";
@@ -18,10 +15,6 @@ const io = new SocketService(server).io;
 const PORT = process.argv[2] || process.env.PORT || 3000;
 
 app.use(cors());
-app.get("/", async (req: Request, res: Response) => {
-  const messages = await prismaClient.message?.findMany();
-  res.json({ success: "ok", messages });
-});
 
 server.listen(PORT, () => {
   console.log(`Server listenings to port ${PORT}: `);
@@ -32,7 +25,6 @@ async function main() {
   io.on("connection", async (socket) => {
     socket.on("send-msg", async (data: any) => {
       await publisher.publish("MESSAGE", data);
-      // io.emit("receive-msg", `New ${data}`);
     });
   });
 
@@ -40,10 +32,8 @@ async function main() {
     io.emit("receive-msg", `Received ${message}`);
     await producer.send({
       topic: "MESSAGES",
-      messages: [
-        {value: message },
-      ],
-    })
+      messages: [{ value: message }],
+    });
   });
 }
 
